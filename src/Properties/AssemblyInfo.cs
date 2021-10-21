8a90428cfee6de07dabd31cfa95c516d7d1a39c2
Minosuko Sylveon <63941519+Minosuko@users.noreply.github.com>
2021-10-21 12:40:33 +0700

    Update AssemblyInfo.cs

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("C-SHARP RSA Crypt EXAMPLE")]
-[assembly: AssemblyDescription("RSA Crypt Example by Catover203")]
+[assembly: AssemblyDescription("RSA Crypt Example by Minosuko")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CatBoom")]
+[assembly: AssemblyCompany("Mino")]
 [assembly: AssemblyProduct("C# RSA EXAMPLE")]
-[assembly: AssemblyCopyright("Copyright Catover203 © CatBoom 2021")]
+[assembly: AssemblyCopyright("Copyright Minosuko © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3a7b79d4-f403-492a-bff9-ee4b3d50602a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
